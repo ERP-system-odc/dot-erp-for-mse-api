08ad87cb2070a954d23cfaae4f1d861336bccaf9
Yohannes Kindu <Yohannes Kindu>
2022-10-28 13:03:04 +0300

    some improvements

--- src/middleware/productValidation.ts
+++ src/middleware/productValidation.ts
@@ -1,8 +1,10 @@
-import { productSchema,usedInventorySchema,productUpdateSchema} from "../validationSchema/productSchema"
+import { productSchema,productUpdateSchema} from "../validationSchema/productSchema"
 
 const Joi=require("joi")
 
+
 export const productDataValidation=async(req,res,next)=>{
+    console.log(req.body)
     const productData={
        
         product_quantity:req.body.product_quantity,
@@ -23,28 +25,28 @@ export const productDataValidation=async(req,res,next)=>{
         req.body.product_expense=parseFloat(productData.product_expense)
         if(!req.body.expense)
         req.body.expense=0
-        const items=req.body["product_items"]
-        if(items.length==0)
-        return res.status(400).json({
-            "status":400,
-            "message":"product quantities are required."
-        })
-        items.forEach(element => {
-            const productItemData={
-               iu_name:element.iu_name,
-               iu_quantity:element.iu_quantity
+        // const items=req.body["product_items"]
+        // if(items.length==0)
+        // return res.status(400).json({
+        //     "status":400,
+        //     "message":"product quantities are required."
+        // })
+    //     items.forEach(element => {
+    //         const productItemData={
+    //            iu_name:element.iu_name,
+    //            iu_quantity:element.iu_quantity
                 
-            }
-            const {error, value} =usedInventorySchema.validate(productItemData,{abortEarly:false})
-            if(error)
-            return res.status(400).json({
-                "status":400,
-                "message":error.message
-            })
+    //         }
+    //         const {error, value} =usedInventorySchema.validate(productItemData,{abortEarly:false})
+    //         if(error)
+    //         return res.status(400).json({
+    //             "status":400,
+    //             "message":error.message
+    //         })
             
-       element.iu_quantity=parseInt(element.iu_quantity)
+    //    element.iu_quantity=parseInt(element.iu_quantity)
             
-        });
+    //     });
         next();
     }
     catch(err){
